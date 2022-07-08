7aa2f0ae4b847bf328d305f8970bf28b478eaa54
Lucas <lucas.182g@gmail.com>
2022-07-08 03:32:01 -0300

    PrimerDesafioCoder

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul className="appUl">
+          <li>Inicio</li>
+          <li>Películas</li>
+          <li>Series</li>
+          <li>Que podemos ver?</li>
+        </ul>
+      </header>
+      <section className="appSection">
+        <h1>Prueba con ReactJS</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
