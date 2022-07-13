fbb9e2bf9ec4c76bb889af7b2e934a7b17208e51
Lucas Gallo <lucas.182g@gmail.com>
2022-07-12 22:29:39 -0300

    SegundoDesafio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <section className="appSection">
+        <h1>Prueba con ReactJS, agregando logo y componente NavBar.js</h1>
+      </section>
     </div>
   );
 }
